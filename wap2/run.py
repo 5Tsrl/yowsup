f4f1ea514f0c619efda0fc3525571452de819e02
aguiraf <aguiraf@gmail.com>
2015-05-01 10:08:20 +0200

    avanzamenti

--- wap2/run.py
+++ wap2/run.py
@@ -4,6 +4,7 @@ from yowsup.layers.auth                        import YowAuthenticationProtocolL
 from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
 from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
 from yowsup.layers.protocol_acks               import YowAckProtocolLayer
+from yowsup.layers.protocol_iq                 import YowIqProtocolLayer
 from yowsup.layers.network                     import YowNetworkLayer
 from yowsup.layers.coder                       import YowCoderLayer
 from yowsup.stacks import YowStack
@@ -12,13 +13,32 @@ from yowsup.layers import YowLayerEvent
 from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS
 from yowsup import env
 
+#raf
+import logging
 
-CREDENTIALS = ("393666538819", "hVN5mCgraHfB+bdQotTpP2Jrg78=") # replace with your phone and password
+logging.basicConfig(filename='wapper.log',level=logging.INFO, format='%(asctime)s %(message)s')
+logging.info('Wapper started...')
+
+
+msgLogger = logging.getLogger("MSG")
+msgLogger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s, %(message)s")
+handler = logging.FileHandler("messaggi.log")
+handler.setFormatter(formatter)
+msgLogger.addHandler(handler)
+
+
+#muletto
+#CREDENTIALS = ("393666538819", "hVN5mCgraHfB+bdQotTpP2Jrg78=") # replace with your phone and password
+#salaregia
+#CREDENTIALS = ("393421884433", "EXJUWBtJfbgEkya/83bCG5zBAIQ=") # replace with your phone and password
+#c3
+CREDENTIALS = ("393666536772", "RqM5uSnbC88FFJeHMefrPzfJ1xo=") # replace with your phone and password
 
 if __name__==  "__main__":
     layers = (
         EtaLayer,
-        (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer)
+        (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer, YowIqProtocolLayer)
     ) + YOWSUP_CORE_LAYERS
 
     stack = YowStack(layers)
